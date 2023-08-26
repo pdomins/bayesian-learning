bae3f953c63b2e869c0be0a0fa7401a306a6339b
Paula Oseroff <oseroffp@gmail.com>
2023-08-26 17:46:55 -0300

    feat(error_functions): add computing classification error from df and k-fold-cross-val

--- error_functions.py
+++ error_functions.py
@@ -1,10 +1,40 @@
-from   typing import Any, Callable
-import pandas as pd
+from   typing     import Any, Callable
+from   data_split import k_fold_n_splits
+from   df_utils   import get_column_value_dict
+import pandas     as pd
+import numpy      as np
 
-def compute_classification_error(df_test : pd.DataFrame, expected_labels : dict[Any, Any], predict : Callable[[pd.Series], Any]):
+
+def compute_classification_error(df_test : pd.DataFrame, expected_labels : dict[Any, Any], predict : Callable[[pd.Series], Any]) -> int:
     classification_error = 0
     for test_sample_idx, test_sample_series in df_test.iterrows():
         expected_label  = expected_labels[test_sample_idx]
         predicted_label = predict(test_sample_series)
         if predicted_label != expected_label : classification_error += 1
     return classification_error
+
+def compute_classification_error_from_df(df : pd.DataFrame, out_label_col : str, predict : Callable[[pd.Series], Any]) -> int:
+    df_out_label_dict = get_column_value_dict(df, out_label_col)
+    df_input          = df.drop(columns=[out_label_col], inplace=False)
+    df_err            = compute_classification_error(df_input, df_out_label_dict, predict)
+    return df_err
+
+def k_fold_cross_validation(df : pd.DataFrame, out_label_col : str, predict : Callable[[pd.Series], Any], k : int, random_state : np.random.Generator = None) -> list[dict[str, Any]]:
+    k_splits = k_fold_n_splits(df, k, random_state=random_state)
+    k_splits_dict_list = []
+    for train_df, test_df in k_splits:
+        train_err = compute_classification_error_from_df(train_df, out_label_col, predict)
+        test_err  = compute_classification_error_from_df(test_df, out_label_col, predict)
+        curr_split_dict = {
+            "train": {
+                "df"  : train_df,
+                "err" : train_err
+            },
+            "test" : {
+                "df"   : test_df,
+                "err"  : test_err
+            }
+        }
+        k_splits_dict_list.append(curr_split_dict)
+    return k_splits_dict_list
+
