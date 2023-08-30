76d6a971b452b5381aab375e9987b76f19bf3128
Paula Oseroff <oseroffp@gmail.com>
2023-08-30 06:08:44 -0300

    refactor(plotter): separate into functions for later impl of multiple roc curves

--- plotter.py
+++ plotter.py
@@ -15,21 +15,31 @@ def plot_confusion_matrix(confusion_matrix : pd.DataFrame, title : str, save_fil
     else:
         plt.show()
 
-def plot_roc_from_positive_rates(positive_rates : list[dict[str, Any]], title : str, save_file : str = None, annotate : bool = False, font_size : int = 5):
-    plt.figure()
-    FPRs, TPRs = np.array(list(map(lambda pr : pr["FPR"], positive_rates))), np.array(list(map(lambda pr : pr["TPR"], positive_rates)))
-    plt.xlim((-0.025, 1.025))
-    plt.ylim((-0.025, 1.025))
+def __add_roc_plot_template__(title : str):
+    plt.xlim((-0.1, 1.1))
+    plt.ylim((-0.1, 1.1))
     plt.ylabel("Tasa de verdaderos positivos")
     plt.xlabel("Tasa de falsos positivos")
     plt.title(title)
     plt.plot(np.array([0, 1]),np.array([0, 1]), linestyle="dashed", color="grey")
-    plt.plot(FPRs, TPRs, color="purple")
-    plt.plot(FPRs, TPRs, marker='o', color="purple")
-    plt.vlines(x = 0, ymin=0, ymax=TPRs.min(), colors="purple")
+
+def add_roc_plot_from_positive_rates(positive_rates : list[dict[str, Any]], annotate : bool = False, font_size : int = 5, color : str = "purple", label : str = None):
+    FPRs, TPRs = np.array(list(map(lambda pr : pr["FPR"], positive_rates))), np.array(list(map(lambda pr : pr["TPR"], positive_rates)))
+    if label is not None:
+        plt.plot(FPRs, TPRs, color=color, label=label)
+    else:
+        plt.plot(FPRs, TPRs, color=color)
+    plt.plot(FPRs, TPRs, marker='o', color=color)
+    plt.vlines(x = 0, ymin=0, ymax=TPRs.min(), colors=color)
     if annotate:
         for i, j in zip(FPRs, TPRs):
             plt.annotate('(%.2g, %.2g)' % (i, j), xy=(i, j), textcoords='offset points', xytext=(0,10), ha='center', fontsize=font_size, color="purple")
+
+def plot_roc_from_positive_rates(positive_rates : list[dict[str, Any]], title : str, save_file : str = None, annotate : bool = False, font_size : int = 5, label : str = None):
+    plt.figure()
+    __add_roc_plot_template__(title)
+    add_roc_plot_from_positive_rates(positive_rates, annotate, font_size, label=label)
+    plt.legend(loc="lower right")
     if save_file is not None:
         plt.savefig(save_file, bbox_inches='tight', dpi=1200)
     else:
