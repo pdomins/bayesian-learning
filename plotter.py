23f018edd7f73112866faefbb3a56bf3d755be69
Paula Oseroff <oseroffp@gmail.com>
2023-08-30 07:12:15 -0300

    feat(plotter): plot multiple ROCs in the same plot

--- plotter.py
+++ plotter.py
@@ -33,12 +33,23 @@ def add_roc_plot_from_positive_rates(positive_rates : list[dict[str, Any]], anno
     plt.vlines(x = 0, ymin=0, ymax=TPRs.min(), colors=color)
     if annotate:
         for i, j in zip(FPRs, TPRs):
-            plt.annotate('(%.2g, %.2g)' % (i, j), xy=(i, j), textcoords='offset points', xytext=(0,10), ha='center', fontsize=font_size, color="purple")
+            plt.annotate('(%.2g, %.2g)' % (i, j), xy=(i, j), textcoords='offset points', xytext=(0,7), ha='center', fontsize=font_size, color=color)
 
-def plot_roc_from_positive_rates(positive_rates : list[dict[str, Any]], title : str, save_file : str = None, annotate : bool = False, font_size : int = 5, label : str = None):
+def plot_roc_from_positive_rates(positive_rates : list[dict[str, Any]], title : str, save_file : str = None, annotate : bool = False, font_size : int = 5, label : str = None, color : str = "purple"):
     plt.figure()
     __add_roc_plot_template__(title)
-    add_roc_plot_from_positive_rates(positive_rates, annotate, font_size, label=label)
+    add_roc_plot_from_positive_rates(positive_rates, annotate, font_size, label=label, color=color)
+    plt.legend(loc="lower right")
+    if save_file is not None:
+        plt.savefig(save_file, bbox_inches='tight', dpi=1200)
+    else:
+        plt.show()
+
+def plot_multiple_roc_fromm_positive_rates(positive_rates_list : list[list[dict[str, Any]]], colors : list[str], title : str, save_file : str = None, annotate : bool = False, font_size : int = 5, labels : list[str] = None):
+    plt.figure()
+    __add_roc_plot_template__(title)
+    for i in range(len(positive_rates_list)):
+        add_roc_plot_from_positive_rates(positive_rates_list[i], annotate, font_size, color=colors[i], label=labels[i])
     plt.legend(loc="lower right")
     if save_file is not None:
         plt.savefig(save_file, bbox_inches='tight', dpi=1200)
